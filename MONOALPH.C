71ed391169f09c89ab10af300dc9e28fc9843a70
Bhavesh Gupta <bhaveshsgupta@gmail.com>
2014-08-11 09:45:43 +0530

    completed

--- MONOALPH.C
+++ MONOALPH.C
@@ -4,8 +4,8 @@
 void main()
 {
 
-	int i;
-	char o[26],cipert[26] ,original[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},j,ciper[26];
+	int i,t;
+	char o[50],bha[50],cipert[26],cipers[50] ,original[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},j,ciper[26];
 	clrscr();
 	randomize();
 //for(i=0; i<=25;i++)
@@ -30,9 +30,39 @@ void main()
 	//printf("\n %c",cipert[i]);
 	}
 	for(i=0;i<=25;i++)
-	printf(" %c",cipert[i]);
+	printf("%c",original[i]);
+	printf("\n");
+	for(i=0;i<=25;i++)
+	printf("%c",cipert[i]);
+	printf("\nenter the string to convert:");
+	gets(o);
+	printf("\n");
 
-
+	for(i=0;o[i]!='\0';i++)
+	{
+	for(j=0;j<26;j++)
+		{
+		if(o[i]==original[j])
+			{
+			cipers[i]=cipert[j];
+			}
+		}
+	}
+	for(i=0;o[i]!='\0';i++)
+	printf("%c",cipers[i]);
+	for(i=0;cipers[i]!='\0';i++)
+	{
+	for(j=0;j<26;j++)
+		{
+		if(cipers[i]==cipert[j])
+			{
+			bha[i]=original[j];
+			}
+		}
+	}
+	printf("\n");
+	for(i=0;o[i]!='\0';i++)
+	printf("%c",bha[i]);
 	getch();
 
 
